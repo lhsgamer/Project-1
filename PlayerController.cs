8def6b39adbc9bf44e8a99764090e8e6a5744df8
lhsgamer <43823598+lhsgamer@users.noreply.github.com>
2021-02-19 10:47:11 -0500

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rb2d;
+    
+
+  
+   
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rb2d.AddForce(movement * speed);
+    }
+
+    
+   
+}
+
